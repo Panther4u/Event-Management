7cc8f4b3d548525c382f4c96fe73c641e81c3d4f
Panther4u <teampanther4@gmail.com>
2024-08-16 14:44:55 +0530

    save

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-        {
-          protocol: "http",
-          hostname: "**",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+  output: 'export',  // Add this line to enable static export
+};
+
+module.exports = nextConfig;
