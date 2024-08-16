d01f7b6c2cf35357c021329278d94926c423397c
Panther4u <teampanther4@gmail.com>
2024-08-16 15:03:17 +0530

    update

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-    ],
-    unoptimized: true,  // Disable image optimization for static exports
-  },
-  output: 'export',  // Enable static export
 };
 
-module.exports = nextConfig;
+module.exports = {
+  images: {
+      remotePatterns: [
+          {
+              protocol: "https",
+              hostname: "**",
+          },
+          {
+              protocol: "http",
+              hostname: "**",
+          },
+      ],
+  },
+};
