c4095173ec92647769fcd9c722cab330a4abfb2b
Panther4u <150365330+Panther4u@users.noreply.github.com>
2024-08-19 20:05:35 +0530

    update

--- client/next.config.js
+++ client/next.config.js
@@ -19,15 +19,12 @@
 
 // module.exports = nextConfig;
 
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-};
-
-module.exports = {
+    trailingSlash: true,
     images: {
+        unoptimized: true, // Disable image optimization for static export
         remotePatterns: [
             {
                 protocol: "https",
@@ -39,4 +36,16 @@ module.exports = {
             },
         ],
     },
+    async exportPathMap(defaultPathMap) {
+        // Exclude pages using getServerSideProps from export
+        const pathMap = { ...defaultPathMap };
+        delete pathMap['/admin/auth'];
+        delete pathMap['/users/signup'];
+        delete pathMap['/users/signin']; // Exclude this page too
+        return pathMap;
+    },
 };
+
+module.exports = nextConfig;
+
+
